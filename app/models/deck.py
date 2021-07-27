76b0dd08b8155dd8056951c6bf07878715c7c149
John Michihara <john.michihara@gmail.com>
2021-07-27 12:33:54 -1000

    Create Deck model with seed file

---
+++ app/models/deck.py
@@ -0,0 +1,29 @@
+from .db import db
+from datetime import datetime
+
+
+class Deck(db.Model):
+    __tablename__ = 'decks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60), nullable=False)
+    description = db.Column(db.String(255))
+    public = db.Column(db.Boolean, nullable=False, default=False)
+    creator_id = db.Column(
+        db.Integer, db.ForeignKey('users.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False,
+                           default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False,
+                           default=datetime.utcnow)
+
+
+def to_dict(self):
+    return {
+        'id': self.id,
+        'title': self.title,
+        'description': self.description,
+        'public': self.public,
+        'creator_id': self.creator_id,
+        'created_at': self.created_at,
+        'updated_at': self.updated_at
+    }
