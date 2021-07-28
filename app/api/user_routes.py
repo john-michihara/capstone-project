a040915a83239eb34520e22da29fe382219e4a3f
John Michihara <john.michihara@gmail.com>
2021-07-28 07:34:46 -1000

    Create api route to get a user's created decks

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, jsonify
 from flask_login import login_required
 from app.models import User
+from app.models import Deck
 
 
 user_routes = Blueprint('users', __name__)
@@ -18,3 +19,11 @@ def users():
 def user(id):
     user = User.query.get(id)
     return user.to_dict()
+
+
+@user_routes.route('/<int:id>/created-decks')
+@login_required
+def user_created_decks(id):
+    decks = Deck.query.filter_by(creator_id=id).all()
+    print(decks)
+    return {'decks': [deck.to_dict() for deck in decks]}
