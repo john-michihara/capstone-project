44bc96958fe0b882f710cf8ec8b549de055930c4
John Michihara <john.michihara@gmail.com>
2021-07-28 09:27:49 -1000

    Fix thunks

--- react-app/src/store/decks.js
+++ react-app/src/store/decks.js
@@ -1,15 +1,9 @@
-const SET_DECKS = 'decks/SET_DECKS'
-const SET_CREATED_DECKS = 'decks/SET_CREATED_DECKS'
+const SET_DECKS = 'decks/SET_DECKS';
 
 const setDecks = (decks) => ({
   type: SET_DECKS,
   decks
 });
-
-const setCreatedDecks = (decks) => ({
-  type: SET_CREATED_DECKS,
-  createdDecks: decks
-})
 
 const initialState = {};
 
@@ -19,15 +13,7 @@ export const getDecks = () => async (dispatch) => {
     const { decks } = await response.json();
     dispatch(setDecks(decks));
   }
-}
-
-export const getCreatedDecks = (userId) => async (dispatch) => {
-  const response = await fetch(`/api/users/${userId}/created`);
-  if (response.ok) {
-    const { decks } = await response.json();
-    dispatch(setCreatedDecks(decks))
-  }
-}
+};
 
 export default function reducer(state = initialState, action) {
   let newState;
@@ -42,15 +28,6 @@ export default function reducer(state = initialState, action) {
         ...state,
         ...newState
       };
-    case SET_CREATED_DECKS:
-      new State = {};
-      action.createdDecks.forEach(deck => {
-        newState[deck.id] = deck;
-      })
-      return {
-        ...state,
-        ...newState
-      }
     default:
       return state;
   }
--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session';
 import decks from './decks';
+import createdDecks from './createdDecks'
 
 const rootReducer = combineReducers({
   session,
-  decks
+  decks,
+  createdDecks
 });
 
 
