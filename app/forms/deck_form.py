4b262fc2e5118dd3bf3d44e1e9b5bb1db68b902c
John Michihara <john.michihara@gmail.com>
2021-07-29 07:20:09 -1000

    Add validations to create deck from

--- app/forms/deck_form.py
+++ app/forms/deck_form.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, IntegerField
-from app.models import Deck, UserDeck
-
-
-# def title_validator(form, field):
-#     title = form.data['title']
-
+from wtforms.validators import DataRequired, Length
 
 
 class CreateDeckForm(FlaskForm):
-    title = StringField('title')
-    description = StringField('description')
+    title = StringField('title', validators=[
+        DataRequired(message='Please enter a title to create your deck.'),
+        Length(max=60, message='Please enter a title that is less than 60 characters.')
+    ])
+    description = StringField('description', validators=[
+        Length(max=255, message='Please enter a description that is less than 255 characters.')
+    ])
     viewable = BooleanField('viewable')
     creatorId = IntegerField('creator_id')
