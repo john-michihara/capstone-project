053615c0af25c5855a1e47154d2f3a19f081cd55
John Michihara <john.michihara@gmail.com>
2021-08-01 20:41:00 -1000

    Add demo button to splash page

--- react-app/src/components/Splash/SplashLeft/index.js
+++ react-app/src/components/Splash/SplashLeft/index.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { login } from '../../../store/session';
 import styles from './SplashLeft.module.css';
 
 const SplashLeft = () => {
+  const dispatch = useDispatch();
+
+  const handleDemo = () => {
+    (async () => {
+      await dispatch(login('marnie@aa.io', 'password'));
+    })();
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.margin}>
         <h1 className={styles.title}>Become your most unstoppable self</h1>
         <p className={styles.subtitle}>Master any subject, one success at a time.</p>
-        <Link to='/login' exact={true}>
-          <button className={styles.button}>Get started</button>
-        </Link>
+        <div>
+          <Link to='/login' exact={true}>
+            <button className={styles.signupButton}>Get started</button>
+          </Link>
+          <button
+            className={styles.demoButton}
+            onClick={handleDemo}
+          >
+            Demo
+          </button>
+        </div>
       </div>
     </div>
   );
