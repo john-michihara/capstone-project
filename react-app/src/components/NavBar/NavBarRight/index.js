349abba197c39a6ef5bc1471916aac872e869a16
John Michihara <john.michihara@gmail.com>
2021-08-04 09:28:44 -1000

    Add conditional rendering to search in nav bar

--- react-app/src/components/NavBar/NavBarRight/index.js
+++ react-app/src/components/NavBar/NavBarRight/index.js
@@ -17,37 +17,42 @@ const NavBarRight = () => {
 
   return (
     <div className={styles.container}>
-      <form
-        className={focus ? styles.focusForm : styles.form}
-        onSubmit={handleSubmit}
-      >
-        <span className={focus ? styles.focusIcon : styles.icon}>
-          <i className="fas fa-search"></i>
-        </span>
-        <input
-          className={styles.input}
-          name='keyword'
-          type='text'
-          placeholder='Search'
-          value={keyword}
-          onChange={e => setKeyword(e.target.value)}
-          onFocus={() => setFocus(true)}
-          onBlur={() => setFocus(false)}
-          required={true}
-        />
-      </form>
-      {user ?
-        (<ProfileDropdown />) :
-        (<div>
+      {user ? (
+        <>
+          <form
+            className={focus ? styles.focusForm : styles.form}
+            onSubmit={handleSubmit}
+          >
+            <span className={focus ? styles.focusIcon : styles.icon}>
+              <i className="fas fa-search"></i>
+            </span>
+            <input
+              className={styles.input}
+              name='keyword'
+              type='text'
+              placeholder='Search'
+              value={keyword}
+              onChange={e => setKeyword(e.target.value)}
+              onFocus={() => setFocus(true)}
+              onBlur={() => setFocus(false)}
+              required={true}
+            />
+          </form>
+          <ProfileDropdown />
+        </>
+      ) : (
+        <div>
           <Link to='/login' exact={true}>
             <button className={styles.login}>Log in</button>
           </Link>
           <Link to='/sign-up'>
             <button className={styles.signup}>Sign up</button>
           </Link>
-        </div>)}
+        </div>
+      )}
 
-    </div>
+
+    </div >
   );
 };
 
