a0c119d0663519bcf6db114788a72d9be5e2799b
John Michihara <john.michihara@gmail.com>
2021-08-05 12:29:47 -1000

    Add profile pic to recent in profile

--- react-app/src/components/Profile/ProfileRecent/index.js
+++ react-app/src/components/Profile/ProfileRecent/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import styles from './ProfileRecent.module.css';
+import ProfiePic from '../../ProfilePic';
 
 const ProfileRecent = () => {
   const userDecks = useSelector(state => Object.values(state.userDecks))
@@ -10,8 +11,11 @@ const ProfileRecent = () => {
       {userDecks.map(deck => (
         <div className={styles.container} key={deck.details.id}>
           <div className={styles.details}>
-            <span className={styles.number}>0 Terms</span>
-            <span className={styles.creator}>{deck.details.creator.username}</span>
+            <div className={styles.number}>0 Terms</div>
+            <div className={styles.imageContainer}>
+              <ProfiePic user={deck.details.creator} />
+            </div>
+            <div className={styles.creator}>{deck.details.creator.username}</div>
           </div>
           <div>
             <span className={styles.title}>{deck.details.title}</span>
